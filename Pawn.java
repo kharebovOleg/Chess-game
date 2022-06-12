450fd53a914bedb5648ca663993ce9114a5beace
oleg_kharebov <xarebov1993@yandex.ru>
2022-06-12 08:45:37 +0300

    fixed

--- Pawn.java
+++ Pawn.java
@@ -1,4 +1,4 @@
-public class Pawn extends ChessPiece{
+public class Pawn extends ChessPiece {
     public Pawn(String color) {
         super(color);
     }
@@ -10,11 +10,11 @@ public class Pawn extends ChessPiece{
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-       if (checkPos(line) && checkPos(column) && checkPos(toLine) && checkPos(toColumn)
-           && chessBoard.board[line][column] != null) {
+       if (checkPos(line) && checkPos(column) && checkPos(toLine) && checkPos(toColumn) && chessBoard.board[line][column] != null) {
            if (column == toColumn) {
                int dir;
                int start;
+
                if (color.equals("White")) {
                    dir = 1;
                    start = 1;
@@ -22,14 +22,18 @@ public class Pawn extends ChessPiece{
                    dir = -1;
                    start = 6;
                }
+
                if (line + dir == toLine) {
                    return chessBoard.board[toLine][toColumn] == null;
                }
+
                if (line == start && line + 2 * dir == toLine) {
                    return chessBoard.board[toLine][toColumn] == null && chessBoard.board[line + dir][column] == null;
                }
+
            } else {
-               if ((column - toColumn == 1 || column - toColumn == -1) && (line - toColumn == 1 || line - toLine == -1)
+
+               if ((column - toColumn == 1 || column - toColumn == -1) && (line - toLine == 1 || line - toLine == -1)
                        && chessBoard.board[toLine][toColumn] != null) {
                    return !chessBoard.board[toLine][toColumn].getColor().equals(color);
                } else return false;
